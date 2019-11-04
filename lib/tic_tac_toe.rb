ff30a6b34b80b87848b584081cbf1183727cd061
Deanna Kelley <deanna.georges@gmail.com>
2019-11-04 02:01:09 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,53 @@
+class TicTacToe
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  WIN_COMBINATIONS = [
+      [0,1,2], #top row 
+      [3,4,5], #middle row 
+      [6,7,8], #bottom row 
+      [0,3,6], #left column
+      [1,4,7], #middle column
+      [2,5,8], #right column
+      [0,4,8], #diagonal from top left
+      [2,4,6]  #diagonal from top right
+    ]
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(user_input)
+    user_input.to_i - 1 
+  end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    if @board[index].nil? || @board[index] == " "
+      false
+    else
+      true
+    end
+  end
+  
+  def valid_move?
+    
+  end
+  
+  def turn 
+    
+    
+  end
+  
+  
+  
+end 
